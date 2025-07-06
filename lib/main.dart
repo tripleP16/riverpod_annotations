7dfa72b5189636d0a1ec156845e884ef9c686557
pperez16 <perez51160900@hotmail.com>
2025-07-06 12:49:15 +0200

    feat: Update MyApp to utilize darkModeProvider for theme management

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotations/config/router/app_router.dart';
 import 'package:riverpod_annotations/config/theme/app_theme.dart';
+import 'package:riverpod_annotations/presentation/providers/state_providers.dart';
 
 void main() => runApp(
       const ProviderScope(
@@ -15,11 +16,12 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final routes = ref.watch(appRouterProvider);
+    final darkTheme = ref.watch(darkModeProvider);
     return MaterialApp.router(
       title: 'Riverpod Providers',
       debugShowCheckedModeBanner: false,
       routerConfig: routes,
-      theme: AppTheme(isDarkmode: false).getTheme(),
+      theme: AppTheme(isDarkmode: darkTheme).getTheme(),
     );
   }
 }
